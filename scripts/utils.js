aab9d6423f2e237ba63edf7c501bb31723a41bfd
FMondragon7 <fernandomondragon07@gmail.com>
2023-06-29 21:49:21 -0700

    feat: add sorting to tasks

--- scripts/utils.js
+++ scripts/utils.js
@@ -42,39 +42,29 @@ export function sortedList(allTasks) {
       sortedList = allTasks.sort((a, b) => a.title.localeCompare(b.title));
       break;
     case "Due date":
-      sortedList = allTasks
-        .sort((a, b) => {
-          if (a.due_date === "" && b.due_date === "") {
-            return 0; // No change in order for tasks without due date
-          }
-          if (a.due_date === "") {
-            return 1; // a comes after b if a doesn't have a due date
-          }
-          if (b.due_date === "") {
-            return -1; // a comes before b if b doesn't have a due date
-          }
+      sortedList = allTasks.sort((a, b) => {
+        if (a.due_date === null && b.due_date === null) {
+          return 0; // No change in order for tasks without due date
+        }
+        if (a.due_date === null) {
+          return 1; // a comes after b if a doesn't have a due date
+        }
+        if (b.due_date === null) {
+          return -1; // a comes before b if b doesn't have a due date
+        }
 
-          const dateA = new Date(a.due_date);
-          const dateB = new Date(b.due_date);
+        const dateA = new Date(a.due_date);
+        const dateB = new Date(b.due_date);
 
-          if (dateA < dateB) {
-            return -1; // a comes before b if a's due date is earlier than b's due date
-          }
-          if (dateA > dateB) {
-            return 1; // a comes after b if a's due date is later than b's due date
-          }
-          return 0; // No change in order for tasks with the same due date
-        })
-        .sort((a, b) => {
-          // Move tasks with due date to the top
-          if (a.due_date !== "" && b.due_date === "") {
-            return -1; // a comes before b if a has a due date and b doesn't
-          }
-          if (a.due_date === "" && b.due_date !== "") {
-            return 1; // a comes after b if b has a due date and a doesn't
-          }
-          return 0; // No change in order for tasks with the same due date status
-        });
+        if (dateA < dateB) {
+          return -1; // a comes before b if a's due date is earlier than b's due date
+        }
+        if (dateA > dateB) {
+          return 1; // a comes after b if a's due date is later than b's due date
+        }
+        return 0; // No change in order for tasks with the same due date
+      });
+
       break;
     case "Importance":
       sortedList = allTasks.sort((a, b) => {
